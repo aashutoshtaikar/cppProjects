7ee28c676f6918336ea9dda217c8872ebf1972c2
aashutoshtaikar <aashutoshtaikar@gmail.com>
2019-01-11 21:23:36 -0800

    added

---
+++ AdvancedCpp/ConstructorsAndMemory/romanToDecimal1.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<memory.h>
+#include<map>
+using namespace std;
+
+/* 
+    roman['I']= 1;
+    roman['V'] = 5;
+    roman['X'] = 10;
+    roman['L'] = 50;
+    roman['C'] = 100;
+    roman['D'] = 500;
+    roman['M']= 1000;
+ */
+int findStr(char c){
+    switch(c){
+        case 'I': return 1;
+        break;
+        case 'V' : return 5;
+        break;
+        case 'X' : return 10;
+        break;
+        case 'L' : return 50;
+        break;
+        case 'C' : return 100;
+        break;
+        case 'D' : return 500;
+        break;
+        case 'M' : return 1000;
+        break;
+        default:
+        return 0;
+    }
+}
+
+int romanToDec(string str){
+
+    
+    int num{0};
+    int sz = str.size() - 1;
+   
+
+    for(int i = 0; i<sz; ) {
+        if(findStr(str[i]) > findStr(str[i+1])){
+            num += findStr(str[i]) + findStr(str[i+1]);
+        }
+        else if (findStr(str[i]) < findStr(str[i+1])){
+            num +=findStr(str[i+1]) - findStr(str[i]);
+        }
+        else if (findStr(str[i]) == findStr(str[i+1])){
+            num +=findStr(str[i]) + findStr(str[i+1]);
+        }
+
+        i = i + 2;
+    }
+
+    if(str.size()%2 != 0){
+        num += findStr(str[sz]);
+    }
+    
+    return num; 
+}
+
+
+int main(){
+    cout << romanToDec("CMXCI") << endl;
+   //cout << findStr('I');
+}
